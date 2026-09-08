8fd6f787a4248373c4de25810f880f3d13ebc81e
“Indy <i.kavelaars@nijmegen.nl>
2026-09-08 15:56:13 +0200

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,15 @@
+import { GemeenteNijmegenCdkApp } from '@gemeentenijmegen/projen-project-type';
+import { javascript } from 'projen';
+const project = new GemeenteNijmegenCdkApp({
+  cdkVersion: '2.189.1',
+  devDeps: ['@gemeentenijmegen/projen-project-type'],
+  name: 'mijn-nieuwe-project',
+  packageManager: javascript.NodePackageManager.NPM,
+  projenrcTs: true,
+
+  // defaultReleaseBranch: "main",  /* The name of the main release branch. */
+  // deps: [],                      /* Runtime dependencies of this module. */
+  // description: undefined,        /* The description is just a string that helps people understand the purpose of the package. */
+  // packageName: undefined,        /* The "name" in package.json. */
+});
+project.synth();
